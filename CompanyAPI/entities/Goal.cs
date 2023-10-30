383dbb4350bddcdf69bc1702d32e4f8bd71ef634
Lukas <vanderspiegel.lukas@gmail.com>
2023-10-30 14:21:55 +0100

    setup entities

---
+++ CompanyAPI/entities/Goal.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyAPI.entities
+{
+    public class Goal
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
