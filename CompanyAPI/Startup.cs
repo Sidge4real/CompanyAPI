9781cbf72bbadd403ad7cf5326ce9bfdd1cf0a92
Lukas <vanderspiegel.lukas@gmail.com>
2023-10-30 14:04:55 +0100

    setup

---
+++ CompanyAPI/Startup.cs
@@ -0,0 +1,23 @@
+﻿namespace CompanyAPI
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+            });
+        }
+    }
+}
