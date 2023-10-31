0821f2ff0fde6166858068c17a85dfe4e79f7d6c
Lukas <vanderspiegel.lukas@gmail.com>
2023-10-31 11:59:19 +0100

    setup services

---
+++ CompanyAPI/services/CompanyData.cs
@@ -0,0 +1,24 @@
+﻿using CompanyAPI.entities;
+
+namespace CompanyAPI.services
+{
+    public interface ICompanyData
+    {
+        IEnumerable<Company> GetAll();
+        Company Get(int id);
+    }
+    public class CompanyData : ICompanyData
+    {
+        private static List<Company> companies;
+        public Company Get(int id)
+        {
+            var company = companies.FirstOrDefault(a => a.Id == id);
+            return company;
+        }
+
+        public IEnumerable<Company> GetAll()
+        {
+            return companies;
+        }
+    }
+}
