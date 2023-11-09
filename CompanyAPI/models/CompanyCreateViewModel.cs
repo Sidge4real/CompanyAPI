c7c221d13facac59189a89177c5f0b15ae089a0e
Lukas <vanderspiegel.lukas@gmail.com>
2023-11-09 15:12:03 +0100

    POST: view model company

---
+++ CompanyAPI/models/CompanyCreateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyAPI.models
+{
+    public class CompanyCreateViewModel
+    {
+        [Required, MaxLength(80)]
+        public string Name { get; set; }
+        [MaxLength(160)]
+        public string Description { get; set; }
+        public long GroupId { get; set; }
+        public string Image { get; set; }
+        public string Sector { get; set; }
+    }
+}
