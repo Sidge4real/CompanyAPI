59db63a9e66e9af0287fb4d7e536e8c4dc3d736e
Lukas <vanderspiegel.lukas@gmail.com>
2023-11-09 19:19:26 +0100

    swagger UI added

--- CompanyAPI/Startup.cs
+++ CompanyAPI/Startup.cs
@@ -10,12 +10,26 @@ namespace CompanyAPI
             services.AddSingleton<ICompanyGroupData, CompanyGroupData>();
             services.AddSingleton<IGoalData, GoalData>();
             services.AddControllers();
+            services.AddSwaggerGen();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("./swagger/v1/swagger.json", "My API v1");
+                    c.RoutePrefix = string.Empty;
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler(new ExceptionHandlerOptions
+                {
+                    ExceptionHandler = context => context.Response.WriteAsync("OOPS")
+                });
             }
             app.UseStaticFiles();
             app.UseRouting();
