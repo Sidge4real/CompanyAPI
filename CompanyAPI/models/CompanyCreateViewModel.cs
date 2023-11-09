cd2c0154beda33a8c3853429c463c57dd7e93094
Lukas <vanderspiegel.lukas@gmail.com>
2023-11-09 16:17:43 +0100

    update data annotation changes

--- CompanyAPI/models/CompanyCreateViewModel.cs
+++ CompanyAPI/models/CompanyCreateViewModel.cs
@@ -4,7 +4,7 @@ namespace CompanyAPI.models
 {
     public class CompanyCreateViewModel
     {
-        [Required(ErrorMessage = "Name is required"), MaxLength(80, ErrorMessage = "Name can have a maximum of 80 characters")]
+        [Required(ErrorMessage = "Name is required"), MaxLength(80, ErrorMessage = "Name can have a maximum of 80 characters")] // data annotation
         public string Name { get; set; }
 
         [MaxLength(160, ErrorMessage = "Description can have a maximum of 160 characters")]
